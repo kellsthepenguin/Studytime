90000db06d9247afef9ae91057fc92d5f4dec248
Kim <kells@kakao.com>
2023-05-29 14:49:43 +0900

    fix favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,7 @@ export default function Document() {
           as='style'
           href='https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/variable/pretendardvariable-dynamic-subset.css'
         />
-        <link rel='icon' href='/favicon.svg' type='image/svg+xml' />
+        <link rel='icon' href='/favicon.png' type='image/svg+xml' />
         <link rel='manifest' href='/manifest.json' />
         <meta
           name='apple-mobile-web-app-status-bar-style'
